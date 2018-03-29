0ee24169a74a2b3bdce1dafd8dde815095c48fc3
l0reas <loreas.clonen@hotmail.be>
2018-03-29 20:01:11 +0200

    idk

--- form.php
+++ form.php
@@ -1,8 +1,26 @@
 <html>
+<head>
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+   integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+   crossorigin="anonymous">
+
+
+
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+
+
+    <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet" type="text/css">
+
+
+    <link href="css/freelancer.min.css" rel="stylesheet">
+</head>
 <body>
 
-Welcome <?php echo $_POST["Name"]; ?><br>
-Your email address is: <?php echo $_POST["Email"]; 
+Welcome <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?>
 
 </body>
 </html>
